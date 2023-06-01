5f445e6d2b857f5f8fec97258948758f08e50ca3
Axel Padilla <adpadillar25@gmail.com>
2023-06-01 11:29:01 -0600

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
+#include <iostream>
+
+#include "../include/caja.h"
+#include "../include/cuadrado.h"
 #include "../include/figura.h"
 #include "../include/rectangulo.h"
-#include "../include/cuadrado.h"
-#include "../include/caja.h"
-
-#include <iostream>
 
 using namespace std;
 
@@ -14,6 +14,7 @@ int main() {
     cout << "Perimetro: " << f1.perimetro() << endl;
     cout << "Area: " << f1.area() << endl;
     cout << "Volumen: " << f1.volumen() << endl;
+    cout << endl;
 
     Rectangulo r1(5.0, 7.0);
 
@@ -21,6 +22,7 @@ int main() {
     cout << "Perimetro: " << r1.perimetro() << endl;
     cout << "Area: " << r1.area() << endl;
     cout << "Volumen: " << r1.volumen() << endl;
+    cout << endl;
 
     Cuadrado c1(5.0);
 
@@ -28,6 +30,7 @@ int main() {
     cout << "Perimetro: " << c1.perimetro() << endl;
     cout << "Area: " << c1.area() << endl;
     cout << "Volumen: " << c1.volumen() << endl;
+    cout << endl;
 
     Caja caja;
 
@@ -35,7 +38,7 @@ int main() {
     cout << "Perimetro: " << caja.perimetro() << endl;
     cout << "Area: " << caja.area() << endl;
     cout << "Volumen: " << caja.volumen() << endl;
-
+    cout << endl;
 
     return 0;
 }
